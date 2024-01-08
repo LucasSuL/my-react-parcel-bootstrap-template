6cc9f389c69a3d5c03ad6bf2bb63ae0ed3cf7125
Lucas Su <lijie.su93@gmail.com>
2024-01-08 19:42:53 +0800

    init commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+
+function App() {
+    return (
+        <div className='App'>
+            <NavBar />
+            <Content />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
